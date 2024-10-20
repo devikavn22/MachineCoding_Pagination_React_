08a960c00fe057c70355119eb57bd28697bed6ce
devikavn22 <devika1997eee@gmail.com>
2024-10-20 17:05:33 +0530

    Pagination in React-- initial commit

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+
+import styles from './Pagination.module.css';
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  return (
+    <div className={styles.pagination}>
+      <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
+        Previous
+      </button>
+      <span>
+        Page {currentPage} of {totalPages}
+      </span>
+      <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </div>
+  )
+}
+
+export default Pagination;
